f95633bcd276abbcf4030f6f3f888801ebe00f60
juan-josue <juansemail2001@gmail.com>
2025-01-25 15:27:08 -0500

    feat: chat history

--- client/memory-lane/src/components/Chat.jsx
+++ client/memory-lane/src/components/Chat.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
 
 function Chat() {
+  const [messages, setMessages] = useState([]);
   const {
     text,
     startListening,
@@ -32,6 +33,13 @@ function Chat() {
         const audioUrl = "http://127.0.0.1:5000/get-audio";
         const audio = new Audio(audioUrl);
         audio.play();
+
+        // Append to messages
+        setMessages((prev) => [
+          ...prev,
+          { role: "user", content: message },
+          { role: "system", content: data.reply },
+        ]);
       } else {
         console.error("Failed to send message", response);
       }
@@ -44,16 +52,53 @@ function Chat() {
     <>
       {hasRecognitionSupport ? (
         <>
-          <div className="flex flex-col gap-10 justify-center items-center">
-            <button className="rounded-full bg-black w-40 h-40" onClick={startListening}>
-              <FontAwesomeIcon className="text-white text-5xl" icon={faMicrophone} />
-            </button>
-            {isListining ? <p className="bg-gray-100 p-3 w-[300px] rounded text-center">The app is using your micmic❗️</p> : null}
-            
-            {text ? (
-              <p className="flex gap-10 p-3 items-center bg-gray-100 rounded">{text} <button className='p-3 w-[100px] bg-black text-white rounded' onClick={() => sendMessage(text)}>Send</button></p>
-              
-            ) : null}
+          <div className="flex flex-row gap-10 w-full justify-center items-center">
+            <div className="flex flex-col gap-10 justify-center items-center w-1/2">
+              <button
+                className="rounded-full bg-black w-40 h-40"
+                onClick={startListening}
+              >
+                <FontAwesomeIcon
+                  className="text-white text-5xl"
+                  icon={faMicrophone}
+                />
+              </button>
+              {isListining ? (
+                <p className="bg-gray-100 p-3 w-[300px] rounded text-center">
+                  The app is using your micmic❗️
+                </p>
+              ) : null}
+
+              {text ? (
+                <p className="flex gap-10 p-3 items-center bg-gray-100 rounded">
+                  {text}
+                  <button
+                    className="p-3 w-[100px] bg-black text-white rounded"
+                    onClick={() => sendMessage(text)}
+                  >
+                    Send
+                  </button>
+                </p>
+              ) : null}
+            </div>
+            <div className="w-1/2">
+              <div className="flex flex-col gap-10 bg-gray-100 p-10 rounded-xl max-h-100 overflow-y-auto">
+                {messages.map((message, index) => {
+                  return (
+                    <p
+                      key={index}
+                      className={`p-3 rounded-lg w-4/5 ${
+                        message.role === "user"
+                          ? "bg-green-100 text-left self-start"
+                          : "bg-gray-200 text-right self-end"
+                      }`}
+                    >
+                      {message.content}
+                    </p>
+                  );
+                })}
+              </div>
+            </div>
           </div>
         </>
       ) : (
